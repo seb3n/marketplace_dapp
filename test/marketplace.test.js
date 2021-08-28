25b4b0ded5252f4a5bb6c7cdd3623e47f7ac371e
pink <8978497+seb3n@users.noreply.github.com>
2021-08-28 12:30:30 -0400

    started rewriting test

--- test/marketplace.test.js
+++ test/marketplace.test.js
@@ -1,4 +1,5 @@
 const contractName = artifacts.require("Marketplace");
+const nftContract = artifacts.require("./ItemNft.sol");
 const { sampleItem, noItems } = require("./testData.js");
 
 contract(contractName.name, (accounts) => {
@@ -6,6 +7,7 @@ contract(contractName.name, (accounts) => {
     let result;
     let expected;
     const [alice, bob] = accounts;
+    let nftContractAddress;
 
 
     // PRE-DEPLOYMENT TESTS
@@ -14,32 +16,46 @@ contract(contractName.name, (accounts) => {
     // contractInstance = await contractName.deployed();
 
     before(async () => {
-        contractInstance = await contractName.new();
+        contractInstance = await contractName.deployed();//.new();
+        nftcContractInstance = await nftContract.deployed();
+        nftContractAddress = nftcContractInstance.address
+
     });
 
-    it("can fetch the list of items", async () => {
-        result = await contractInstance.getItems();
-        expect(result).to.be.empty;
-        // assert.deepEqual(result, expected, "No items should be shown.");
-    });
+    // it("can fetch the list of items", async () => {
+    //     // result = await contractInstance.getItems();
+    //     // contractInstance.idToMarketIte
+    //     expect(result).to.be.empty;
+    //     // assert.deepEqual(result, expected, "No items should be shown.");
+    // });
 
 
     describe('creating an item', async () => {
         before(`create an item using alice's account (${alice})`, async () => {
-            result = await contractInstance.create(
-                sampleItem.title,
-                sampleItem.description,
-                sampleItem.price,
-                sampleItem.attached_media,
-                sampleItem.tag,
+            // result = await contractInstance.createMarketItem(
+            //     address(0x0),
+            //     1,
+            //     10,
+            //     { from: alice }
+            // ); // This acount should come from ganache.
+            // console.log(result);
+        });
+        it("can transfer ownership to contract", async () => { 
+            console.log(nftContractAddress);
+            // console.log(nftcContractInstance);
+            result = await contractInstance.createMarketItem(
+                nftcContractInstance.address,
+                1,
+                10,
                 { from: alice }
             ); // This acount should come from ganache.
+            console.log(result);
         });
-        it("can get items with one item in list", async () => {
-            result = await contractInstance.getItems();
-            expected = [newItem];
-            expect(result).to.deep.equal(expected);
-            // assert.deepEqual(result, expected, "The owner of the item should be the first account.");
-        });
+        // it("can get items with one item in list", async () => {
+        //     result = await contractInstance.getItems();
+        //     expected = [newItem];
+        //     expect(result).to.deep.equal(expected);
+        //     // assert.deepEqual(result, expected, "The owner of the item should be the first account.");
+        // });
     });
 });
