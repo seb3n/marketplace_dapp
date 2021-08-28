6f4f5a7f21a4eafd0be74b7689deb325f9aed3c0
pink <8978497+seb3n@users.noreply.github.com>
2021-08-27 21:53:27 -0400

    implemented test for itemNFT contract

---
+++ test/itemNft.test.js
@@ -0,0 +1,54 @@
+const contractName = artifacts.require("ItemNft");
+
+contract(contractName.name, (accounts) => {
+    const [alice, bob] = accounts;
+    let contract;
+    let tokenID;
+    let totalSupply;
+    before('deploy cotract', async () => {
+        contract = await contractName.new(); //.deployed();
+    });
+
+    describe('deployment', async () => {
+        it('deploys successfully', async () => {
+
+            const address = contract.address;
+            console.log(address);
+            assert.notEqual(address, '');
+            assert.notEqual(address, 0x0);
+            assert.notEqual(address, undefined);
+            assert.notEqual(address, null);
+        });
+        it('has a name', async () => {
+            const name = await contract.name();
+            console.log(name);
+            assert.equal(name, 'Our MarketPlace')
+        }); it('has a symbol', async () => {
+            const symbol = await contract.symbol();
+            console.log(symbol);
+            assert.equal(symbol, 'OMP')
+        });
+    })
+
+    describe('createToken', async () => {
+
+        before(`Create a token using alice account: ${alice}`, async () => {
+            tokenID = await contract.createToken('google.com', { from: alice });
+            // totalSupply = await contract.
+            console.log(tokenID);
+            const event = tokenID.logs[0].args;
+            // console.log(event);
+            // console.log(event.tokenId.toNumber());
+        });
+        it('Can get token ID', async () => {
+            const tokenEvent = tokenID.logs[0].args;
+            const id = tokenEvent.tokenId.toNumber()
+            const to = tokenEvent.to;
+            const from = tokenEvent.from;
+            assert.equal(id, 1);
+            assert.equal(to, alice,'the correct address was not found');
+            assert.equal(from, 0x0);
+
+        });
+    });
+});
