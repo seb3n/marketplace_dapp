d0a842877f809bbf5bbd9f9243799eb1d068010a
pink <8978497+seb3n@users.noreply.github.com>
2021-08-27 21:50:52 -0400

    migration updated to pass the nft contract addr

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 var Marketplace = artifacts.require("Marketplace");
+var ItemNFT = artifacts.require("ItemNft");
 
 module.exports = function (deployer) {
-    deployer.deploy(Marketplace);
+    deployer.deploy(Marketplace).then(function () {
+        return deployer.deploy(ItemNFT, Marketplace.address);
+    });
+
 }
